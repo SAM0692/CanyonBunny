63db2cbe8cb600506353b1c4ea09401cedc0fea5
SAM0692 <samuelam0692@gmail.com>
2016-08-01 10:36:21 -0400

    Update DesktopLauncher.java

--- desktop/src/com/packtpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/com/packtpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -2,10 +2,24 @@ package com.packtpub.libgdx.canyonbunny.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.packtpub.libgdx.canyonbunny.CanyonBunnyMain;
 
 public class DesktopLauncher {
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+	
 	public static void main (String[] arg) {
+		if(rebuildAtlas) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "assets-raw/images", "android/assets/images", "canyonbunny.pack");
+		}
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 480;
