bc6615ef0ccd4dbaa84c5bd94fec7ffceaf1f1a5
SAM0692 <samuelam0692@gmail.com>
2016-08-01 10:50:08 -0400

    Update Constants.java

--- core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
+++ core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     // Visible game world is 5 meters wide and 5 meters tall
     public static final float VIEWPORT_WIDTH = 5.0f;
     public static final float VIEWPORT_HEIGHT = 5.0f;
+    
+    // Location of the description file for the texture atlas
+    public static final String TEXTURE_ATLAS_OBJECTS = "images/canyonbunny.pack";
 }
