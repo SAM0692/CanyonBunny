1d47d3321fdca1b06464cae783faebf4cd92a0e6
SAM0692 <samuelam0692@gmail.com>
2016-08-08 10:00:28 -0400

    Update Rock.java

--- core/src/com/packtpub/libgdx/canyonbunny/game/objects/Rock.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Rock.java
@@ -1,1 +1,42 @@
+package com.packtpub.libgdx.canyonbunny.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.packtpub.libgdx.canyonbunny.game.Assets;
+
+public class Rock extends AbstractGameObject {
+  private TextureRegion regEdge;
+  private TextureRegion regMiddle;
+  
+  private int length;
+  
+  public Rock() {
+    init();
+  }
+  
+  private void init() {
+    dimension.set(1, 1.5f);
+    
+    regEdge = Assets.instance.rock.edge;
+    regMiddle = Assets.instance.rock.middle;
+    
+    // Start length of this rock
+    setLength(1);
+  }
+  
+  public void setLength(int length) {
+    this.length = length;
+  }
+  
+  public void increaseLength(int amount) {
+    setLength(length + amount);
+  }
+}
+
+
+
+
+
+
+
 
