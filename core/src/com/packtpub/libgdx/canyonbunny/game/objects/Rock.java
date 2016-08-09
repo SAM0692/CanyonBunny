62658df808324f49d07da5dc1c069d980001463f
SAM0692 <samuelam0692@gmail.com>
2016-08-09 10:59:16 -0400

    Update Rock.java

--- core/src/com/packtpub/libgdx/canyonbunny/game/objects/Rock.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Rock.java
@@ -32,28 +32,37 @@ public class Rock extends AbstractGameObject {
     setLength(length + amount);
   }
   
-  // Draw left edge
-  reg = regEdge;
-  relX -= dimension.x / 4;
-  batch.draw(reg.getTexture(), position.x + relX, position.y + relY, origin.x, origin.y, dimension.x / 4, dimension.y,
-              scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegiony(), reg.getRegionWidth(), reg.getRegionHeight(),
-              false, false);
-              
-  // Draw middle
-  reg = regMiddle;
-  relX = 0;
-  for(int i = 0; i < length; i++){
-    batch.draw(reg.getTexture(), position.x + relX, position.y + relY, origin.x, origin.y, dimension.x, dimension.y,
-              scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegiony(), reg.getRegionWidth(), reg.getRegionHeight(),
-              false, false);
-              relX += dimension.x;
+  @Override
+  public void render(SpriteBatch batch){
+    TextureRegion reg = null;
+    
+    float relX = 0;
+    float relY = 0;
+    
+     // Draw left edge
+    reg = regEdge;
+    relX -= dimension.x / 4;
+    batch.draw(reg.getTexture(), position.x + relX, position.y + relY, origin.x, origin.y, dimension.x / 4, dimension.y,
+                scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegiony(), reg.getRegionWidth(), reg.getRegionHeight(),
+                false, false);
+                
+    // Draw middle
+    reg = regMiddle;
+    relX = 0;
+    for(int i = 0; i < length; i++){
+      batch.draw(reg.getTexture(), position.x + relX, position.y + relY, origin.x, origin.y, dimension.x, dimension.y,
+                scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegiony(), reg.getRegionWidth(), reg.getRegionHeight(),
+                false, false);
+                relX += dimension.x;
+    }
+    
+    // Draw right edge
+    reg = regEdge;
+    batch.draw(reg.getTexture(), position.x + relX, position.y + relY, origin.x + dimension.x / 8, origin.y, dimension.x / 4, dimension.y,
+                scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegiony(), reg.getRegionWidth(), reg.getRegionHeight(),
+                true, false);
   }
-  
-  // Draw right edge
-  reg = regEdge;
-  batch.draw(reg.getTexture(), position.x + relX, position.y + relY, origin.x + dimension.x / 8, origin.y, dimension.x / 4, dimension.y,
-              scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegiony(), reg.getRegionWidth(), reg.getRegionHeight(),
-              true, false);
+ 
 }
 
 
