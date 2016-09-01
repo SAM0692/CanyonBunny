d4286189bddffb11c4dec26314a588001b3d6655
SAM0692 <samuelam0692@gmail.com>
2016-09-01 09:33:59 -0400

    Update Feather.java

--- core/src/com/packtpub/libgdx/canyonbunny/game/objects/Feather.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Feather.java
@@ -1,1 +1,46 @@
+package com.packtpub.libgdx.canyonbunny.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.packtpub.libgdx.canyonbunny.game.Assets;
+
+public class Feather extends AbstractGameObject {
+  
+  private TextureRegion regFeather;
+  
+  public boolean collected;
+  
+  public Feather() {
+    init();
+  }
+  
+  public void init() {
+    dimension.set(0.5f, 0.5f);
+    
+    regFeather = Assets.instance.feather.feather;
+    
+    // Set bounds for collision detection
+    bounds.set(0, 0, dimension.x, dimension.y);
+    
+    collected = false;
+  }
+  
+  public void render(SpriteBatch batch) {
+    if(collectec) {
+      return;
+    }
+    
+    TextureRegion reg = regFeather;
+    batch.draw(reg.getTexture(), position.x, position.y, origin.x, origin.y, dimension.x, dimension.y,
+                scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), reg.getRegionY(),
+                reg.getRegionWidth(), reg.getRegionHeight(), false, false);
+  }
+  
+  public int getScore() {
+    return 250;
+  }
+}
+
+
+
 
